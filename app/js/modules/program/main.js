1739ced616e2d45bbc4c2c0ae2aa60b0e1820a7c
chriszarate <chris@zarate.org>
2015-01-04 23:15:31 -0500

    Require VCC maps data.

--- app/js/modules/program/main.js
+++ app/js/modules/program/main.js
@@ -4,6 +4,7 @@ module.exports = function (Module, App, Backbone) {
 
   'use strict';
 
+  require('./data/vcc-maps-data.js')(Module, App, Backbone);
   require('./models/session-model.js')(Module, App, Backbone);
   require('./models/session-detail-model.js')(Module, App, Backbone);
   require('./views/session-view.js')(Module, App, Backbone);
